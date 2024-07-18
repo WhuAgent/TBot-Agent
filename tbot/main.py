cf5f21f8718551c576eb0e0eeaa4b664bc5b7a7d
zski7n803 <1242775894@qq.com>
2024-07-18 11:38:35 +0800

    add window

--- tbot/main.py
+++ tbot/main.py
@@ -13,5 +13,5 @@ def run_task(task):
 
 if __name__ == '__main__':
     # code, res = capture("winword")
-    run_task('请帮我打开文档 "C:\\Users\\lornd\\Desktop\\TBot\\origin.docx" 和文档 "C:\\Users\\lornd\\Desktop\\TBot\\res.docx"，并将 origin.docx 中的所有内容复制到 res.docx 中')
+    run_task('请帮我在激活记事本中激活1.txt这个窗口，判断它是否存在，最大化这个窗口，查看这个时候窗口的大小，将其改到宽1000，高800.然后移动这个窗口到x坐标为0，y坐标为10的位置，将他置顶，获取窗口名称、类名、路径、进程，最后关闭这个窗口')
     # run_task("请帮我打开文档 'C:\\Users\\lornd\\Desktop\\res.docx'，并向其中写入内容 'hello'，最后关闭文件")
