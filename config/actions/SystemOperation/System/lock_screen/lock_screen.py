034b90be95d9d12672a065d11dece1074b85a7b3
廖叶飞 <1206232012@qq.com>
2024-07-22 14:09:10 +0800

    修改了代码结构，还没有进行测试

---
+++ config/actions/SystemOperation/System/lock_screen/lock_screen.py
@@ -0,0 +1,19 @@
+import yaml
+
+from utils.function import function_args2str, function_rets2str
+from utils.function import convert2double_slash_path
+
+config_path = "config/actions/SystemOperation/System/lock_screen/lock_screen.yaml"
+with open(config_path, "r", encoding="UTF-8") as f:
+    config = yaml.safe_load(f)
+
+
+def decorate_args(args):
+    # 在这里写下每个命令参数的特殊处理
+    return args
+
+
+def lock_screen(args):
+    args = decorate_args(args)
+    args_str = function_args2str(config, args)
+    return f"SystemAction({args_str})"
