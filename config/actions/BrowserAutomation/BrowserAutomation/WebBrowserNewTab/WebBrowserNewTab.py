082e80728c3cd8742aeb75a9d2629e9edfd90cb7
prchen <1254517839@qq.com>
2024-07-23 15:20:51 +0800

    浏览器自动化prompt

---
+++ config/actions/BrowserAutomation/BrowserAutomation/WebBrowserNewTab/WebBrowserNewTab.py
@@ -0,0 +1,22 @@
+
+import os.path
+
+import yaml
+
+from utils.function import function_args2str, function_rets2str
+
+config_path = os.path.join(os.path.dirname(__file__), "WebBrowserNewTab.yaml")
+with open(config_path, "r", encoding="UTF-8") as f:
+    config = yaml.safe_load(f)
+
+
+def decorate_args(args):
+    # 在这里写下每个命令参数的特殊处理
+    return args
+
+
+def WebBrowserNewTab(args):
+    args = decorate_args(args)
+    args_str = function_args2str(config, args)
+    # rets_str = function_rets2str(config, args)
+    return f"WebBrowserNewTab({args_str})"
---
+++ config/actions/BrowserAutomation/BrowserAutomation/WebBrowserTakeShot/WebBrowserTakeShot.py
@@ -0,0 +1,33 @@
+
+import os.path
+
+import yaml
+
+from utils.function import function_args2str, function_rets2str, convert2double_slash_path
+
+config_path = os.path.join(os.path.dirname(__file__), "WebBrowserTakeShot.yaml")
+with open(config_path, "r", encoding="UTF-8") as f:
+    config = yaml.safe_load(f)
+
+
+def decorate_args(args):
+    # 在这里写下每个命令参数的特殊处理
+    file_path = convert2double_slash_path(args["file_path"])
+    args["file_path"] = f'\"{file_path}\"'
+    x = args.get("x")
+    y = args.get("y")
+    width = args.get("width")
+    height = args.get("height")
+    args["scope"] = f'\"[{x},{y},{width},{height}]\"'
+    args.pop("x")
+    args.pop("y")
+    args.pop("width")
+    args.pop("height")
+    return args
+
+
+def WebBrowserTakeShot(args):
+    args = decorate_args(args)
+    args_str = function_args2str(config, args)
+    rets_str = function_rets2str(config, args)
+    return f"WebBrowserTakeShot({args_str})"
