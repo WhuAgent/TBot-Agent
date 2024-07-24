ed9891d2b1a4a326a5f3eef6300ecc9de1b03769
廖叶飞 <1206232012@qq.com>
2024-07-24 10:17:25 +0800

    修改命令的命名为驼峰命名法并与命令规则命名一致，修改测试文件和mian文件，增加返回值

--- tbot/main.py
+++ tbot/main.py
@@ -13,21 +13,5 @@ def run_task(task):
 
 if __name__ == '__main__':
     # code, res = capture("winword")
-    # test_file
-    run_task('检测文件"D:\\Awork\\AI-agent\\rpa-test\\test.csv"是否存在，'
-             '获取文件夹"D:\\Awork\\AI-agent\\rpa-test"文件列表，'
-             '获取文件夹"D:\\Awork\\AI-agent\\rpa-test"文件夹列表，'
-             '读取"D:\\Awork\\AI-agent\\rpa-test\\test.csv"中的数据，'
-            #  '压缩"D:\\Awork\\AI-agent\\rpa-test\\test.csv"文件到文件夹"D:\\Awork\\AI-agent\\rpa-test"，'
-            #  '压缩"D:\\Awork\\AI-agent\\rpa-test\\test"文件夹到文件夹"D:\\Awork\\AI-agent\\rpa-test"，'
-             '重命名"D:\\Awork\\AI-agent\\rpa-test\\test"为 "666",'
-             )
-    
-    # test_system
-    # run_task('执行powershell 命令calc , '
-    #         '执行命令行 calc,'
-    #         '获取系统文件夹路径,'
-    #         '获取用户文件夹路径,'
-    #         '获取临时文件夹路径,'
-    #         '锁屏'
-    #         )
+    run_task('请帮我打开文档 "C:\\Users\\lornd\\Desktop\\TBot\\origin.docx" 和文档 "C:\\Users\\lornd\\Desktop\\TBot\\res.docx"，并将 origin.docx 中的所有内容复制到 res.docx 中')
+    # run_task("请帮我打开文档 'C:\\Users\\lornd\\Desktop\\res.docx'，并向其中写入内容 'hello'，最后关闭文件")
