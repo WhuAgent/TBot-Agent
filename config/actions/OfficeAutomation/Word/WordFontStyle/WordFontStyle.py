cfaf68520f16201edd39c5deaf5265ae6d5c88a8
shimamura <2542907915@qq.com>
2024-10-26 15:51:58 +0800

    Agent知识化：文档格式设置部分

--- config/actions/OfficeAutomation/Word/WordFontStyle/WordFontStyle.py
+++ config/actions/OfficeAutomation/Word/WordFontStyle/WordFontStyle.py
@@ -1,18 +1,32 @@
 import yaml
 
-from utils.function import function_args2str, function_rets2str
+from tbot.utils.function import function_args2str, function_rets2str, check_obj_defined
 
-config_path = "config/actions/OfficeAutomation/Word/WordFontStyle/WordFontStyle.yaml"
+config_path = "tbot/config/actions/OfficeAutomation/Word/WordFontStyle/WordFontStyle.yaml"
 with open(config_path, "r", encoding="UTF-8") as f:
     config = yaml.safe_load(f)
 
 
-def decorate_args(args):
+def decorate_args(args, vars):
     # 在这里写下每个命令参数的特殊处理
+    document = args.get("document", None)
+    bold = args.get("bold", None)
+    italic = args.get("italic", None)
+    underline = args.get("underline", None)
+
+    if document is None or not check_obj_defined(document, vars):
+        raise TypeError(
+            "输入的文档对象应当为一个已定义的对象变量，请确保先打开了一个文档，并且使用内存中已经定义的变量名")
+    else:
+        args["document"] = document
+        args["bold"] = bold
+        args["italic"] = italic
+        args["underline"] = underline
+
     return args
 
 
-def WordFontStyle(args):
-    args = decorate_args(args)
+def WordFontStyle(args, vars):
+    args = decorate_args(args, vars)
     args_str = function_args2str(config, args)
     return f"WordFontStyle({args_str})"
