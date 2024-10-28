b973b3e9a7471e517b4c60accb28306f7bd3b920
zhuyuhan2333 <zhuyuhan2333@qq.com>
2024-10-28 20:06:29 +0800

    feat: tbot新配置优化WordDocumentAgent

--- config/actions/OfficeAutomation/Word/WordGetFilePath/WordGetFilePath.py
+++ config/actions/OfficeAutomation/Word/WordGetFilePath/WordGetFilePath.py
@@ -1,21 +1,21 @@
 import yaml
 
-from tbot.utils.function import function_args2str, function_rets2str, convert2double_slash_path
-
-config_path = "tbot/config/actions/OfficeAutomation/Word/WordGetFilePath/WordGetFilePath.yaml"
-with open(config_path, "r", encoding="UTF-8") as f:
-    config = yaml.safe_load(f)
+from tbot.services import BaseService
+from tbot.utils.function import function_args2str, function_rets2str, check_obj_defined
 
 
-def decorate_args(args):
-    # 在这里写下每个命令参数的特殊处理
-    file_path = convert2double_slash_path(args["file_path"])
-    args["file_path"] = f'\"{file_path}\"'
-    return args
+class WordGetFilePath(BaseService):
+    def __init__(self, config_path):
+        super().__init__(config_path)
 
+    def forward(self, args, vars):
+        document = args.get("document", None)
 
-def WordGetFilePath(args, vars):
-    args = decorate_args(args)
-    args_str = function_args2str(config, args)
-    rets_str = function_rets2str(config, args)
-    return f"{rets_str} = WordGetFilePath({args_str})"
+        if document is None or not check_obj_defined(document, vars):
+            raise TypeError("需要获取文件路径的文档对象应当为一个已打开的文档对象，请确保该文档已经被打开")
+
+        args_str = function_args2str(self.config, args)
+
+        result = f"WordGetFilePath({args_str})"
+        message = f"成功获取文件路径的文档 {document}"
+        return result, message
