91d1b759f8a83bff3c41410e38ba90a268963217
shimamura <2542907915@qq.com>
2024-11-01 20:21:45 +0800

    BaseService版本更改：文档格式设置部分

--- agent.py
+++ agent.py
@@ -154,6 +154,11 @@ class WordTextContentAgent(OperationAgent):
         super().__init__(config, logger)
 
 
+class WordFontSetAgent(OperationAgent):
+    def __init__(self, config, logger):
+        super().__init__(config, logger)
+
+
 class CodeExecutionAgent(BaseAgent):
     def __init__(self, config, logger):
         super().__init__(config, logger)
